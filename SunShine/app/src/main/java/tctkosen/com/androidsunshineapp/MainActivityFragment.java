37ebc26de6b2705205e3b2521159101948acb2a1
talhakosen <talhakosen@hotmail.com>
2016-05-26 23:41:11 +0300

    1.0.9 attach adapter

--- SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivityFragment.java
+++ SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivityFragment.java
@@ -1,11 +1,12 @@
 package tctkosen.com.androidsunshineapp;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,12 +38,17 @@ public class MainActivityFragment extends Fragment {
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
         // use it to populate the ListView it's attached to.
         mForecastAdapter = new ArrayAdapter<>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_forecast, // The name of the layout ID.
-                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
+                getActivity(), // The current context (this activity)
+                R.layout.list_item_forecast, // The name of the layout ID.
+                R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                weekForecast);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+        
         return rootView;
     }
 }
