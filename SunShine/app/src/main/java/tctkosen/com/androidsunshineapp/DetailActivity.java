54299c8b7af0c2fea38dd700c3e4e9a1ea022240
talhakosen <talhakosen@hotmail.com>
2016-05-28 00:20:22 +0300

    3.1.4 add share intent to detail activity

--- SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/DetailActivity.java
+++ SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/DetailActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    String forecast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +19,33 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        String forecast =getIntent().getExtras().get(Intent.EXTRA_TEXT).toString();
+        forecast =getIntent().getExtras().get(Intent.EXTRA_TEXT).toString();
         TextView detailText = (TextView)findViewById(R.id.txt_detail);
         detailText.setText(forecast);
     }
 
+    private void createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + FORECAST_SHARE_HASHTAG);
+        startActivity(shareIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            createShareForecastIntent();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
