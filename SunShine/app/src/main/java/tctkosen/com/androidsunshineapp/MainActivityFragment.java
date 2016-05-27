1b47d83994dbb492b1ac1c2d8512af175dcc6573
talhakosen <talhakosen@hotmail.com>
2016-05-27 23:44:23 +0300

    3.0.9 get location from shared pref

--- SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivityFragment.java
+++ SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivityFragment.java
@@ -1,9 +1,11 @@
 package tctkosen.com.androidsunshineapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
@@ -266,8 +268,11 @@ public class MainActivityFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("34330");
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
