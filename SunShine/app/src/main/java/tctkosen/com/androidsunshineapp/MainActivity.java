da9a4848dce8d3c774f6b37f300d874d1bf0b1ea
talhakosen <talhakosen@hotmail.com>
2016-05-27 11:31:01 +0300

    2.0.4 inflate menu item to activity

--- SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivity.java
+++ SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/MainActivity.java
@@ -3,6 +3,8 @@ package tctkosen.com.androidsunshineapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
