8997a3443eaed291c80e0965e5c638109f516659
talhakosen <talhakosen@hotmail.com>
2016-05-27 09:33:35 +0300

    2.0.1 create http request

---
+++ SunShine/app/src/main/java/tctkosen/com/androidsunshineapp/Config.java
@@ -0,0 +1,8 @@
+package tctkosen.com.androidsunshineapp;
+
+/**
+ * Created by tctkosen on 27/05/16.
+ */
+public class Config {
+    public static String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=1ca986f97558e0ff0d0e043ab018f8ba";
+}
